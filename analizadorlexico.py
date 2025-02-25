5aea2b782bc25425d1bee061191bc7d2f6fc218b
FGachuz <fabian.gutierrez5066@alumnos.udg.mx>
2025-02-24 22:19:46 -0600

    .

--- analizadorlexico.py
+++ analizadorlexico.py
@@ -1,6 +1,13 @@
 # Fase 1. Analizador Léxico
 # 02/24/2025
 # Integrantes: Fabian Gutierrez Gachuz y Maria Dolores Cervantes Araujo
-#Hola me emborache demasiado que se me cai el pantalon :)
-#ya por fin me funciona, gracias
+from lark import Lark
+from lark.exceptions import UnexpectedInput
+import re
 import tkinter
+
+traduccion_tokens = {
+
+    
+}
+
