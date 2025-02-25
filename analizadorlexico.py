2298daa9a010f6dfee70652053d6f44c4b6e142c
FGachuz <fabian.gutierrez5066@alumnos.udg.mx>
2025-02-24 22:15:14 -0600

    hola

--- analizadorlexico.py
+++ analizadorlexico.py
@@ -1,5 +1,6 @@
 # Fase 1. Analizador Léxico
 # 02/24/2025
 # Integrantes: Fabian Gutierrez Gachuz y Maria Dolores Cervantes Araujo
-
+#Hola me emborache demasiado que se me cai el pantalon :)
+#ya por fin me funciona, gracias
 import tkinter
