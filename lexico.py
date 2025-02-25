2966091a48631bd9d649612851c736636fbaf46d
FGachuz <fabian.gutierrez5066@alumnos.udg.mx>
2025-02-24 23:02:39 -0600

    .

--- lexico.py
+++ lexico.py
@@ -1,3 +1,12 @@
-for lark import lark
+from lark import Lark
 
-with open ("")
+with open ("EBNF.ebnf", "r") as file:
+        grammar = file.read()
+
+try:
+    parser = Lark(grammar, parser="lair")
+    print("La gramatica es valida")
+
+except Exception:
+    print("La gramatica es no valida")
+
