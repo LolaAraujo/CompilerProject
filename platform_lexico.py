e4844386b4b93dd15184aec16e53dc624bbdf700
LolaAraujo <99374772+LolaAraujo@users.noreply.github.com>
2025-02-24 22:51:10 -0600

    create file

--- platform_lexico.py
+++ platform_lexico.py
@@ -1,14 +1,11 @@
 # Fase 1. Analizador Léxico
 # 02/24/2025
 # Integrantes: Fabian Gutierrez Gachuz y Maria Dolores Cervantes Araujo
-from lark import Lark
-from lark.exceptions import UnexpectedInput
-import re
 
-import tkinter
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
 
-traduccion_tokens = {
 
-    
-}
+
 
