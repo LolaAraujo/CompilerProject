2cef02ded7f6ceeec087b10f5ed495e0a6137ef9
FGachuz <fabian.gutierrez5066@alumnos.udg.mx>
2025-02-25 19:26:21 -0600

    .

--- lexico.py
+++ lexico.py
@@ -1,6 +1,6 @@
 from lark import Lark
 
-with open ("Gachuz.ebnf", "r") as file:
+with open ("prueba.ebnf", "r") as file:
         grammar = file.read()
 
 try:
@@ -9,7 +9,4 @@ try:
 
 except Exception:
     print("La gramatica es no valida")
-
-try:
-    sentencia = " x > 0"
 
