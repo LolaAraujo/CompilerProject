dbb2030146e2b89e9d59963aa00eed93166aae5b
FGachuz <fabian.gutierrez5066@alumnos.udg.mx>
2025-02-25 20:28:58 -0600

    Se arreglo la gramatica prueba (que es la gramatica de Gachuz pero cambiada para lark)

--- lexico.py
+++ lexico.py
@@ -1,12 +1,15 @@
 from lark import Lark
+from lark.exceptions import UnexpectedInput
+import re
 
-with open ("prueba.ebnf", "r") as file:
-        grammar = file.read()
+
+with open("prueba.ebnf", "r") as file:
+    grammar = file.read()
 
 try:
-    parser = Lark(grammar, parser="lair")
-    print("La gramatica es valida")
-
-except Exception:
-    print("La gramatica es no valida")
+    parser = Lark(grammar, parser="lalr")
+    print("✅ La gramática es válida y compatible con Lark")
+except Exception as e:
+    print("❌ Error en la gramática:")
+    print(e)
 
