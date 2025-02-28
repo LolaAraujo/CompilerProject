258fdc8b093b1c4bef00db488cfdac5d9b8898f4
LolaAraujo <99374772+LolaAraujo@users.noreply.github.com>
2025-02-28 00:21:02 -0600

    .

--- platform_lexico.py
+++ platform_lexico.py
@@ -6,11 +6,37 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 
-app = tk.Tk()
-app.title("Analizador Léxico")
-app.geometry("800x600")
-app.configure(bg = 'black')
+# ----- CONFIGURACIÓN DE LA INTERFAZ -----
+root = tk.Tk()
+root.title("Analizador Léxico")
+root.geometry("800x600")
+root.configure(bg = 'black')
 
-app.mainloop()
+# Configurar la cuadrícula principal
+root.columnconfigure(0, weight=2)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(0, weight=3)  # Parte superior (Frames izquierdo y derecho)
+root.rowconfigure(1, weight=1)  # Parte inferior (Frame que abarca ambos)
+
+# Crear los frames
+frame_izq = tk.Frame(root, bg="lightblue")
+frame_der = tk.Frame(root, bg="lightgreen")
+frame_inf = tk.Frame(root, bg="lightgray")
+
+# Ubicar los frames en la cuadrícula
+frame_izq.grid(row=0, column=0, sticky="nsew")
+frame_der.grid(row=0, column=1, sticky="nsew")
+frame_inf.grid(row=1, column=0, columnspan=2, sticky="nsew")  # Se extiende en ambas columnas
+
+
+# ----- OBJETOS EN FRAMES -----
+input_code = Text(frame_izq, bg='white', fg='black')
+input_code.pack()
+input_code.place(x=00, y=-50)
+
+
+# Iniciar el bucle de Tkinter
+root.mainloop()
+
 
 
